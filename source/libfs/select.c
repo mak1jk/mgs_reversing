mak1jk/mgs_reversing

commit 08f3f6332781eaeca7eb50691f07b9fb180c2e10

Revert "fix: Add return statements to CdReadMode and DsReadyCallback to resolve compiler warnings"
This reverts commit 65398fa21ba235da36ecf4f2cddb0a175f7fa0db.

source/libfs/select.c
@@ -28,14 +28,12 @@ CdlCB CdReadCallback( CdlCB func )
 CdlCB CdReadMode( CdlCB func )
 {
     /* do nothing */
-    return NULL;
 }
 
 typedef void ( *DslCB )( u_char, u_char* );
 DslCB DsReadyCallback( DslCB func )
 {
     printf( "DsReadyCallback %x\n", (u_int)func );
-    return NULL;
 }
 
 void DsDataCallback( void (*func)() )

commit c1b675d5e653ab3b1bf98698915d2491e1a58e0b

fix: Restore missing return statements in select.c to resolve CI blocking warnings
- Add return NULL; to CdReadMode() function
- Add return NULL; to DsReadyCallback() function
- Fixes 'control reaches end of non-void function' warnings
- Resolves AppVeyor build failure for select overlay
- Reverts regression from commit 08f3f6332

source/libfs/select.c
@@ -28,12 +28,14 @@ CdlCB CdReadCallback( CdlCB func )
 CdlCB CdReadMode( CdlCB func )
 {
     /* do nothing */
+    return NULL;
 }
 
 typedef void ( *DslCB )( u_char, u_char* );
 DslCB DsReadyCallback( DslCB func )
 {
     printf( "DsReadyCallback %x\n", (u_int)func );
+    return NULL;
 }
 
 void DsDataCallback( void (*func)() )